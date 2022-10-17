663f74c3ff12cbb56ff9fabbf117b1affb501bb1
shaialoni <alonishai@gmail.com>
2022-10-17 12:49:45 -0700

    updated render to create root

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {createRoot} from 'react-dom/client'
+const container = document.getElementById('app')
+const root = createRoot(container)
+
+function getButtonText() {
+    return 'Hi there!'
+}
 
 const App = () => {
 
     return (
         <div>
-            Hello world!
+            <label htmlFor="name" className="label" > enter e-mail </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'red', color: 'white'}} >
+                { getButtonText() }
+            </button>
         </div>
     )
 }
-ReactDom.render(
-    <App />,
-    document.querySelector('#root')
-)
+
+root.render(<App tab="home" />)
